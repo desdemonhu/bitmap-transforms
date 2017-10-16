fd017e810b1172440a1dc77e4663ead7c7b2c423
Raegan Millhollin <desdemonhu@gmail.com>
2017-10-16 15:06:41 -0700

    manipulation works, just need to figure out how to do invert

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
 const fileChange = require('./lib/file-exchange.js');
+const transform = require('./lib/transform.js');
 
-fileChange('./test/assets/hunt-you-down.bmp');
+fileChange('./test/assets/hunt-you-down.bmp', './test/assets/hunt-redo.bmp', transform.invert);
