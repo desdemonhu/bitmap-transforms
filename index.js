ab7d6c9a2c44f38ed08ef53dc8436c0f57fe521c
Raegan Millhollin <desdemonhu@gmail.com>
2017-11-08 11:54:12 -0800

    Oh man I made it worse

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ const transform = require('./lib/transform.js');
 fileChange('./test/assets/hunt-you-down.bmp', './test/assets/hunt-redo.bmp', transform.invert);
 
 fileChange('./test/assets/hunt-you-down.bmp', './test/assets/hunt-greyscale.bmp', transform.greyscale);
+
+fileChange('./test/assets/hunt-you-down.bmp', './test/assets/hunt-colorshift.bmp', transform.colorShift);
+
+fileChange('./test/assets/house.bmp', './test/assets/house-greyscale.bmp', transform.greyscale);
+
+fileChange('./test/assets/house.bmp', './test/assets/house-colorshift.bmp', transform.colorShift);
+
+fileChange('./test/assets/bitmap.bmp', './test/assets/bitmap-greyscale.bmp', transform.greyscale);
